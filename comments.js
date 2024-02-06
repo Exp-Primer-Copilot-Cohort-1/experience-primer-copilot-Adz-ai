9a0451a7f8ae1408121d5da444f3b7d2ed44f206
Adarssh Athithan <62515833+Adz-ai@users.noreply.github.com>
2024-02-06 17:38:37 +0000

    copilot third commit

--- comments.js
+++ comments.js
@@ -1,11 +1,31 @@
-const http = require('http');
+// create web server and listen to port 8080
+// return comments in JSON format
+var http = require('http');
+var comments = require('./comments.json');
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!');
-});
+http.createServer(function(request, response) {
+  response.writeHead(200, {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*'
+  });
+  response.end(JSON.stringify(comments));
+}).listen(8080);
+console.log('Server is running at http://localhost:8080/');
+```
+comments.json
+```json
+[
+  {
+    "id": 1,
+    "author": "Pete Hunt",
+    "text": "This is one comment"
+  },
+  {
+    "id": 2,
+    "author": "Jordan Walke",
+    "text": "This is *another* comment"
+  }
+]
+```
 
-server.listen(3000, 'localhost', () => {
-    console.log('Server running at http://localhost:3000/');
-});
+###
